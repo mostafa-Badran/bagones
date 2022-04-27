6b2346aafc998697dd5301469554ba080339dd6c
mostafa badran <mostafa.ite@gmail.com>
2022-04-28 02:13:06 +0400

    smallfix

--- resources/views/subCategories/edit.blade.php
+++ resources/views/subCategories/edit.blade.php
@@ -11,15 +11,15 @@
         </h3>
         <div class="card-toolbar">
             <div class="example-tools justify-content-center">
-            <a href="{{ url('subCategory') }}" class="btn btn-secondary">Go Back</a>
+                <a href="{{ url('subCategory') }}" class="btn btn-secondary">Go Back</a>
             </div>
         </div>
     </div>
-    @if ($errors->any())
+    @if($errors->any())
         <div class="alert alert-danger">
             <strong>Whoops!</strong> There were some problems with your input.<br><br>
             <ul>
-                @foreach ($errors->all() as $error)
+                @foreach($errors->all() as $error)
                     <li>{{ $error }}</li>
                 @endforeach
             </ul>
@@ -35,20 +35,48 @@
                     <label>Parent Category<span class="text-danger">*</span></label>
                     <div class=" col-lg-12 col-md-12 col-sm-12">
                         <select class="form-control kt-select2 select2" id="kt_select2_1" name="parent_id">
-                                <option value="{{$category['parent_id']}}">{{$category->get_parent->name}}</option>
+                            <option value="{{ $category['parent_id'] }}">
+                                {{ $category->get_parent->name }}</option>
                         </select>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <label>Name_en<span class="text-danger">*</span></label>
-                    <input type="text" name="name" value="{{ $category->name }}" required class="form-control" placeholder="Enter Name"/>
+                    <input type="text" name="name" value="{{ $category->name }}" required class="form-control"
+                        placeholder="Enter Name" />
                     <span class="form-text text-muted">Please enter Name, Max 50 character allowed</span>
                 </div>
                 <div class="col-lg-4">
                     <label>Name_local<span class="text-danger">*</span></label>
-                    <input type="text" name="name_local" value="{{ $category->name_locale }}" class="form-control" placeholder="Enter Local Name"/>
+                    <input type="text" name="name_local" value="{{ $category->name_locale }}" class="form-control"
+                        placeholder="Enter Local Name" />
                     <span class="form-text text-muted">Please enter Local Name, Max 50 character allowed</span>
                 </div>
+
+                <div class="col-lg-4">
+                    <label>Image<span class="text-danger">*</span></label>
+                    <div class="image-input image-input-outline" id="kt_image_4">
+                        <div class="image-input-wrapper"
+                            style="background-image: url(/uploads/category/{{ $category->image }})"></div>
+                        <label class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
+                            data-action="change" data-toggle="tooltip" title="" data-original-title="Change avatar">
+                            <i class="fa fa-pen icon-sm text-muted"></i>
+                            <input type="file" name="image" accept=".png, .jpg, .jpeg" />
+                            <input type="hidden" name="profile_avatar_remove" />
+                        </label>
+
+                        <span class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
+                            data-action="cancel" data-toggle="tooltip" title="Cancel avatar">
+                            <i class="ki ki-bold-close icon-xs text-muted"></i>
+                        </span>
+
+                        <span class="btn btn-xs btn-icon btn-circle btn-white btn-hover-text-primary btn-shadow"
+                            data-action="remove" data-toggle="tooltip" title="Remove avatar">
+                            <i class="ki ki-bold-close icon-xs text-muted"></i>
+                        </span>
+                    </div>
+                </div>
+
             </div>
         </div>
         <div class="card-footer">
@@ -69,33 +97,65 @@
 {{-- Scripts Section --}}
 @section('scripts')
 <script type="text/javascript">
-
     // CSRF Token
     // var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-    $(document).ready(function(){
+    $(document).ready(function () {
 
-      $( ".select2" ).select2({
-        ajax: {
-          url: "{{url('api/category/dataAjax')}}",
-          type: "post",
-          dataType: 'json',
-          delay: 250,
-          data: function (params) {
-            return {
-                '_token': '{{ csrf_token() }}',
-              'search' : params.term // search term
-            };
-          },
-          processResults: function (response) {
-            return {
-              results: response
-            };
-          },
-          cache: true
-        }
+        $(".select2").select2({
+            ajax: {
+                url: "{{ url('api/category/dataAjax') }}",
+                type: "post",
+                dataType: 'json',
+                delay: 250,
+                data: function (params) {
+                    return {
+                        '_token': '{{ csrf_token() }}',
+                        'search': params.term // search term
+                    };
+                },
+                processResults: function (response) {
+                    return {
+                        results: response
+                    };
+                },
+                cache: true
+            }
 
-      });
+        });
 
     });
+
+    var avatar4 = new KTImageInput('kt_image_4');
+
+    avatar4.on('cancel', function (imageInput) {
+        swal.fire({
+            title: 'Image successfully canceled !',
+            type: 'success',
+            buttonsStyling: false,
+            confirmButtonText: 'Awesome!',
+            confirmButtonClass: 'btn btn-primary font-weight-bold'
+        });
+    });
+
+    avatar4.on('change', function (imageInput) {
+        swal.fire({
+            title: 'Image successfully changed !',
+            type: 'success',
+            buttonsStyling: false,
+            confirmButtonText: 'Awesome!',
+            confirmButtonClass: 'btn btn-primary font-weight-bold'
+        });
+    });
+
+    avatar4.on('remove', function (imageInput) {
+        swal.fire({
+            title: 'Image successfully removed !',
+            type: 'error',
+            buttonsStyling: false,
+            confirmButtonText: 'Got it!',
+            confirmButtonClass: 'btn btn-primary font-weight-bold'
+        });
+    });
+
 </script>
 @endsection
