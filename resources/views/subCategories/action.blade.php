bf70195e9c5c0b174242475c93158ffc1cdeb268
mostafa badran <mostafa.ite@gmail.com>
2022-04-27 21:51:54 +0400

    small fix

--- resources/views/subCategories/action.blade.php
+++ resources/views/subCategories/action.blade.php
@@ -1,7 +1,7 @@
-<a href="{{ route('subCategory',$id) }}" class="btn btn-sm btn-clean btn-icon" title="View details">
+<a href="{{ url('subCategory',$id) }}" class="btn btn-sm btn-clean btn-icon" title="View details">
     <i class="la la-eye"></i>
 </a>
-<a href="{{ route('subCategory/edit',$id) }}" class="btn btn-sm btn-clean btn-icon" title="Edit details">
+<a href="{{ url('subCategory/edit',$id) }}" class="btn btn-sm btn-clean btn-icon" title="Edit details">
     <i class="la la-edit"></i>
 </a>
 <a href="javascript:void(0)" data-id="{{ $id }}" class="delete btn btn-sm btn-clean btn-icon" title="Delete">
