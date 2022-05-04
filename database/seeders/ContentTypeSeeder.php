9f2b63609332670d2739ee165fbaad0bddc686cf
mostafa badran <mostafa.ite@gmail.com>
2022-05-04 15:06:08 +0400

    upload menu aside

--- database/seeders/ContentTypeSeeder.php
+++ database/seeders/ContentTypeSeeder.php
@@ -15,12 +15,34 @@ class ContentTypeSeeder extends Seeder
     public function run()
     {
        
-        $content_types = [ ['name' =>'offer' ], ['name' =>'category'] , ['name' =>'sub category'] , ['name' =>'item'] , ['name' =>'store']];
-        $content_types = json_encode($content_types);
-        $content_types = json_decode($content_types, true);
-        $content_types = array_chunk($content_types, 1000);
-        for ($i = 1; $i < (int)ceil(count($content_types) / 1000); $i++) {
-            Content_type::insert($content_types[$i]);
-        }
+        // $content_types = [ ['name' =>'offer' ], ['name' =>'category'] , ['name' =>'sub category'] , ['name' =>'item'] , ['name' =>'store']];
+        // $content_types = json_encode($content_types);
+        // $content_types = json_decode($content_types, true);
+        // $content_types = array_chunk($content_types, 1000);
+        // for ($i = 1; $i < (int)ceil(count($content_types) / 1000); $i++) {
+        //     Content_type::insert($content_types[$i]);
+        // }
+
+        Content_type::create([
+            'name' => 'offer'
+        ]);
+
+        Content_type::create([
+            'name' => 'category'
+        ]);
+        Content_type::create([
+            'name' => 'sub category'
+        ]);
+
+       Content_type::create([
+            'name' => 'The Empire Strikes Back'
+        ]);
+        Content_type::create([
+            'name' => 'item'
+        ]);
+
+        Content_type::create([
+            'name' => 'store'
+        ]);
     }
 }
