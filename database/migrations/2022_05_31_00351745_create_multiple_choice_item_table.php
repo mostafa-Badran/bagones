26f181e3a0c252e2916b3198f40a14a4047d159e
mostafa badran <mostafa.ite@gmail.com>
2022-06-02 16:58:43 +0400

    item attribute multipule choice compulsory choice

--- database/migrations/2022_05_31_00351745_create_multiple_choice_item_table.php
+++ database/migrations/2022_05_31_00351745_create_multiple_choice_item_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMultipuleChoiceItemTable extends Migration
+class CreateMultipleChoiceItemTable extends Migration
 {
     /**
      * Run the migrations.
